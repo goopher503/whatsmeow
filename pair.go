ca40f77bab7d7e8452dca6314f2bb9ec8c300f18
goopher503 <goopher503@gmail.com>
2026-07-05 09:29:07 +0800

    merge

--- pair.go
+++ pair.go
@@ -193,7 +193,7 @@ func (cli *Client) handlePair(ctx context.Context, deviceIdentityBytes []byte, r
 
 	cli.Store.Account = proto.Clone(&deviceIdentity).(*waAdv.ADVSignedDeviceIdentity)
 
-	if linkCache := cli.phoneLinkingCache; linkCache != nil && linkCache.keyPair != nil {
+	if linkCache := cli.phoneLinkingCache.Load(); linkCache != nil && linkCache.keyPair != nil {
 		cli.Store.PairingEphemeralKey = linkCache.keyPair
 	}
 
